f65fe00f77e809463c6bc53c5e40b4042fd4759b
Nguyễn Viết Đạt <113585061+datbacninh99@users.noreply.github.com>
2024-01-29 15:28:25 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import ExUseState from './component/ExUseState';
-import ExContext from './component/ExContext';
 
 function App() {
   return (
     <div className="App">
       <ExUseState></ExUseState>
-      <ExContext></ExContext>
     </div>
   );
 }
